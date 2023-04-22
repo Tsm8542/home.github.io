b097813a6a8a749158b031fd32a4fb6dffb40b48
Tsm8542 <shaikhabid932@gmail.com>
2023-04-22 20:29:00 +0530

    22-04-2023 08:28 PM

---
+++ script.js
@@ -0,0 +1,335 @@
+var stech = 
+[
+    /*1*/{rep_name:"music.github.io",rep_url:"https://tsm8542.github.io/music.github.io/",title:"Music Player-27748",rep_created:"20 May,2022",rep_lst_upd:"20 May,2022",rep_src:"https://www.github.com/tsm8542/music.github.io/"},
+    /*2*/{rep_name:"stt.github.io",rep_url:"https://tsm8542.github.io/stt.github.io/",title:"At your Service Sir!!!",rep_created:"22 Jun,2022",rep_lst_upd:"22 Jun,2022",rep_src:"https://www.github.com/tsm8542/stt.github.io/"},
+    /*3*/{rep_name:"work-time.github.io",rep_url:"https://tsm8542.github.io/work-time.github.io/",title:"Work-time",rep_created:"23 Jun,2022",rep_lst_upd:"23 Jun,2022",rep_src:"https://www.github.com/tsm8542/work-time.github.io/"},
+    /*4*/{rep_name:"wix.github.io",rep_url:"https://tsm8542.github.io/wix.github.io/",title:"Wix",rep_created:"24 May,2022",rep_lst_upd:"30 Jun,2022",rep_src:"https://www.github.com/tsm8542/wix.github.io/"},
+    /*5*/{rep_name:"modern-periodic-table.github.io",rep_url:"https://tsm8542.github.io/modern-periodic-table.github.io/",title:"Modern Periodic Table",rep_created:"21 Jun,2022",rep_lst_upd:"4 Jul,2022",rep_src:"https://www.github.com/tsm8542/modern-periodic-table.github.io/"},
+    /*6*/{rep_name:"code-editor.github.io",rep_url:"https://tsm8542.github.io/code-editor.github.io/",title:"Document",rep_created:"2 Jul,2022",rep_lst_upd:"5 Jul,2022",rep_src:"https://www.github.com/tsm8542/code-editor.github.io/"},
+    /*7*/{rep_name:"math-formula.github.io",rep_url:"https://tsm8542.github.io/math-formula.github.io/",title:"Maths",rep_created:"12 Jul,2022",rep_lst_upd:"12 Jul,2022",rep_src:"https://www.github.com/tsm8542/math-formula.github.io/"},
+    /*8*/{rep_name:"html.github.io",rep_url:"https://tsm8542.github.io/html.github.io/",title:"HTML-Hypertext Markup Language",rep_created:"4 Aug,2022",rep_lst_upd:"11 Aug,2022",rep_src:"https://www.github.com/tsm8542/html.github.io/"},
+    /*9*/{rep_name:"css.github.io",rep_url:"https://tsm8542.github.io/css.github.io/",title:"CSS-Cascading Style Sheets",rep_created:"6 Aug,2022",rep_lst_upd:"13 Aug,2022",rep_src:"https://www.github.com/tsm8542/css.github.io/"},
+    /*10*/{rep_name:"math-topics.github.io",rep_url:"https://tsm8542.github.io/math-topics.github.io/",title:"Maths-topics",rep_created:"12 Aug,2022",rep_lst_upd:"16 Sep,2022",rep_src:"https://www.github.com/tsm8542/math-topics.github.io/"},
+    /*11*/{rep_name:"chemistry-topics.github.io",rep_url:"https://tsm8542.github.io/chemistry-topics.github.io/",title:"Chemistry-topics",rep_created:"17 Sep,2022",rep_lst_upd:"21 Sep,2022",rep_src:"https://www.github.com/tsm8542/chemistry-topics.github.io/"},
+    /*12*/{rep_name:"to-do.github.io",rep_url:"https://tsm8542.github.io/to-do.github.io/",title:"To-do list",rep_created:"8 Oct,2022",rep_lst_upd:"8 Oct,2022",rep_src:"https://www.github.com/tsm8542/to-do.github.io/"},
+    /*13*/{rep_name:"math-formulas.github.io",rep_url:"https://tsm8542.github.io/math-formulas.github.io",title:"Formula",rep_created:"9 Oct,2022",rep_lst_upd:"17 Oct,2022",rep_src:"https://www.github.com/tsm8542/math-formulas.github.io/"},
+    /*14*/{rep_name:"datetime.github.io",rep_url:"https://tsm8542.github.io/datetime.github.io/",title:"Time",rep_created:"17 Oct,2022",rep_lst_upd:"17 Oct,2022",rep_src:"https://www.github.com/tsm8542/datetime.github.io/"},
+    /*15*/{rep_name:"physics-topics.github.io",rep_url:"https://tsm8542.github.io/physics-topics.github.io/",title:"Physics-topics",rep_created:"17 Sep,2022",rep_lst_upd:"10 Nov,2022",rep_src:"https://www.github.com/tsm8542/physics-topics.github.io/"},
+    /*16*/{rep_name:"search.github.io",rep_url:"https://tsm8542.github.io/search.github.io/",title:"Eazy-Surfer",rep_created:"4 Dec,2022",rep_lst_upd:"4 Dec,2022",rep_src:"https://www.github.com/tsm8542/search.github.io/"},
+    /*17*/{rep_name:"math.github.io",rep_url:"https://tsm8542.github.io/math.github.io",title:"Maths",rep_created:"19 Sep,2022",rep_lst_upd:"19 Dec,2022",rep_src:"https://www.github.com/tsm8542/math.github.io/"},
+    /*18*/{rep_name:"brainstorming.github.io",rep_url:"https://tsm8542.github.io/brainstorming.github.io/",title:"File Manipulation",rep_created:"23 Jun,2022",rep_lst_upd:"21 Dec,2022",rep_src:"https://www.github.com/tsm8542/brainstorming.github.io/"},
+    /*19*/{rep_name:"chemistry.github.io",rep_url:"https://tsm8542.github.io/chemistry.github.io/",title:"Chemistry",rep_created:"19 Dec,2022",rep_lst_upd:"21 Dec,2022",rep_src:"https://www.github.com/tsm8542/chemistry.github.io/"},
+    /*20*/{rep_name:"textbooks.github.io",rep_url:"https://tsm8542.github.io/textbooks.github.io/",title:"Textbooks",rep_created:"28 Dec,2022",rep_lst_upd:"28 Dec,2022",rep_src:"https://www.github.com/tsm8542/textbooks.github.io/"},
+    /*21*/{rep_name:"tts.github.io",rep_url:"https://tsm8542.github.io/tts.github.io/",title:"TTS",rep_created:"3 Jan,2023",rep_lst_upd:"9 Jan,2023",rep_src:"https://www.github.com/tsm8542/tts.github.io/"},
+    /*22*/{rep_name:"file-manipulation.github.io",rep_url:"https://tsm8542.github.io/file-manipulation.github.io/",title:"Project-Manipulator",rep_created:"27 Mar,2023",rep_lst_upd:"27 Mar,2023",rep_src:"https://www.github.com/tsm8542/file-manipulation.github.io/"},
+    //*23*/{rep_name:"",rep_url:"https://tsm8542.github.io/",title:"",rep_created:"",rep_lst_upd:"",rep_src:""},
+    //*24*/{rep_name:"",rep_url:"https://tsm8542.github.io/",title:"",rep_created:"",rep_lst_upd:"",rep_src:""},
+    //*25*/{rep_name:"",rep_url:"https://tsm8542.github.io/",title:"",rep_created:"",rep_lst_upd:"",rep_src:""},
+    //*26*/{rep_name:"",rep_url:"https://tsm8542.github.io/",title:"",rep_created:"",rep_lst_upd:"",rep_src:""},
+    //*27*/{rep_name:"",rep_url:"https://tsm8542.github.io/",title:"",rep_created:"",rep_lst_upd:"",rep_src:""},
+    //*28*/{rep_name:"",rep_url:"https://tsm8542.github.io/",title:"",rep_created:"",rep_lst_upd:"",rep_src:""},
+    //*29*/{rep_name:"",rep_url:"https://tsm8542.github.io/",title:"",rep_created:"",rep_lst_upd:"",rep_src:""},
+    //*30*/{rep_name:"",rep_url:"https://tsm8542.github.io/",title:"",rep_created:"",rep_lst_upd:"",rep_src:""},
+];
+
+var rep_name = [];
+var unsort_rep_name=[];
+var lst_5_res = [];
+var full_res = [];
+
+function assign_rep_name(){
+    for(let i=0;i<stech.length;i++){
+        rep_name[i]=stech[i].rep_name;
+        let temp = rep_name[i];
+        let x = "";
+        for(let m=0;m<temp.length;m++){
+            if(temp[m]=='.'){
+                break;
+            }
+            else{
+                x+=temp[m];
+            }
+        }
+        rep_name[i]=x;
+        unsort_rep_name[i]=x;
+    };
+    rep_name.sort();
+}
+assign_rep_name();
+
+function oninp(){
+    full_res = [];
+    let result=[];
+    document.querySelector(".res").style.display = "block";
+    document.querySelector(".res").innerHTML="";
+    let src_f = document.querySelector("#searcher").value;
+    let src = src_f.toLowerCase();
+    for(let i=0;i<rep_name.length;i++){
+        let temp = rep_name[i];
+        if(src[0]==temp[0]){
+            let src_2 = "";
+            let temp_2 = "";
+            for(let m=0;m<src.length;m++){
+                src_2+=src[m];
+                temp_2+=temp[m];
+            }
+            if(src_2==temp_2){
+                result[i]=temp;
+            }
+        }
+    }
+    if(result.length!=0){
+        result = result.filter(n=>n);
+    }
+    for(let i=0;i<5;i++){
+        if(result[i]!=undefined){
+            let x = "";
+            for(let m=0;m<stech.length;m++){
+                if(result[i]+".github.io"==stech[m].rep_name)
+                x = stech[m].rep_url;
+            }
+            let tres = '<div>'+result[i]+'<a href="'+x+'" target="_blank"><button style="float:right;"><i class="fa fa-globe"></i></button></div>';
+            document.querySelector(".res").innerHTML+=tres;
+            lst_5_res[i] = result[i];
+        }
+        else{
+            lst_5_res[i] = 0;
+        }
+    }
+    for(let i=0;i<result.length;i++){
+        full_res[i] = result[i];
+    }
+    lst_5_res = lst_5_res.filter(n=>n);
+}
+
+function clr(){
+    document.querySelector("#searcher").value="";
+    document.querySelector(".res").innerHTML="";
+}
+
+function fill(){
+    for(let i=0;i<stech.length;i++){
+        let temp = '<tr><td id="'+unsort_rep_name[i]+'" style="text-align:left;" onclick="single_rep(\''+unsort_rep_name[i]+'\')">'+stech[i].title+'</td><td style="text-align:left;"><a href="'+stech[i].rep_src+'" target="_blank">'+stech[i].rep_name+'</a></td><td>'+stech[i].rep_created+'</td><td>'+stech[i].rep_lst_upd+'</td><td><a href="'+stech[i].rep_url+'" target="_blank"><button><i class="fa fa-globe"></i></button></a><button id="'+unsort_rep_name[i]+'pinbtn" onclick="set_mark(\''+unsort_rep_name[i]+'\')">';
+        let x;
+        if(localStorage.getItem(unsort_rep_name[i]+'pstat')=="true"){
+            x = '<i class="fa fa-bookmark"></i></button></td></tr>';
+        }
+        else{
+            x = '<i class="fa fa-bookmark-o"></i></button></td></tr>';
+        }
+        temp+=x;
+        document.querySelector("#all_rep").innerHTML+=temp;
+    }
+    cr_add_notes();
+}
+
+function upd_fill(){
+    document.querySelector("#all_rep").innerHTML='<tr><th>Name</th><th>Repository</th><th>Created</th><th>Last Updated</th><th>Action</th></tr>';
+    for(let i=0;i<stech.length;i++){
+        let temp = '<tr><td id="'+unsort_rep_name[i]+'" style="text-align:left;" onclick="single_rep(\''+unsort_rep_name[i]+'\')">'+stech[i].title+'</td><td style="text-align:left;"><a href="'+stech[i].rep_src+'" target="_blank">'+stech[i].rep_name+'</a></td><td>'+stech[i].rep_created+'</td><td>'+stech[i].rep_lst_upd+'</td><td><a href="'+stech[i].rep_url+'" target="_blank"><button><i class="fa fa-globe"></i></button></a><button id="'+unsort_rep_name[i]+'pinbtn" onclick="set_mark(\''+unsort_rep_name[i]+'\')">';
+        let x;
+        if(localStorage.getItem(unsort_rep_name[i]+'pstat')=="true"){
+            x = '<i class="fa fa-bookmark"></i></button></td></tr>';
+        }
+        else{
+            x = '<i class="fa fa-bookmark-o"></i></button></td></tr>';
+        }
+        temp+=x;
+        document.querySelector("#all_rep").innerHTML+=temp;
+    }
+}
+
+function s(event){
+    let x = event.which;
+    if(x == 13){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function search(event){
+    let bool_check = s(event);
+    if(bool_check){
+        document.querySelector(".src_result").style.display = "block";
+        document.querySelector(".main").style.display = "none";
+        document.querySelector(".res").style.display = "none";
+        document.querySelector(".container-for-single").style.display = "none";
+        document.querySelector(".pin").style.display = "none";
+        let src_term = document.querySelector("#searcher").value;
+        document.querySelector("#src_head").innerHTML = 'Search results for "'+src_term+'" :';
+        let n = '<tr><th>Name</th><th>Repository</th><th>Created</th><th>Last Updated</th><th>Action</th></tr>';
+        if(full_res.length!=0){
+            for(let i=0;i<full_res.length;i++){
+                for(let m=0;m<stech.length;m++){
+                    if(full_res[i]+".github.io"==stech[m].rep_name){
+                        let temp = '<tr><td style="text-align:left;" onclick="single_rep_s(\''+full_res[i]+'\')">'+stech[m].title+'</td><td style="text-align:left;"><a href="'+stech[m].rep_src+'" target="_blank">'+stech[m].rep_name+'</a></td><td>'+stech[m].rep_created+'</td><td>'+stech[m].rep_lst_upd+'</td><td><a href="'+stech[m].rep_url+'" target="_blank"><button><i class="fa fa-globe"></i></button></a><button id="s'+full_res[i]+'pinbtn" onclick="set_mark_s(\''+full_res[i]+'\')">';
+                        if(localStorage.getItem(full_res[i]+'pstat')=="true"){
+                            x = '<i class="fa fa-bookmark"></i></button></td></tr>';
+                        }
+                        else{
+                            x = '<i class="fa fa-bookmark-o"></i></button></td></tr>';
+                        }
+                        temp+=x;
+                        n+=temp;
+                    }
+                }
+            }
+        }
+        else{
+            n = 'Sorry no results found for search term : "'+src_term+'"';
+        }
+        document.querySelector("#src_res").innerHTML=n;
+    }
+}
+
+function toggle_home(){
+    document.querySelector(".src_result").style.display = "none";
+    document.querySelector(".container-for-single").style.display = "none";
+    document.querySelector(".pin").style.display = "none";
+    document.querySelector(".main").style.display = "block";
+    show_all();
+}
+
+function cr_add_notes(){
+    for(let i=0;i<unsort_rep_name.length;i++){
+        if(localStorage.getItem(unsort_rep_name[i])==undefined){
+            localStorage.setItem(unsort_rep_name[i],"");
+            localStorage.setItem(unsort_rep_name[i]+"pstat","false");
+        }
+    }
+}
+
+function single_rep(rep){
+    let curr_rep;
+    for(let i=0;i<stech.length;i++){
+        if(rep+".github.io"==stech[i].rep_name){
+            curr_rep = stech[i];
+        }
+    }
+    document.querySelector(".rep_name").innerHTML = 'Repository: '+curr_rep.rep_name;
+    document.querySelector(".rep_title").innerHTML = 'Title: '+curr_rep.title;
+    document.querySelector(".rep_created").innerHTML = 'Created on: '+curr_rep.rep_created;
+    document.querySelector(".rep_lstupd").innerHTML = 'Last updated: '+curr_rep.rep_lst_upd;
+    document.querySelector(".rep_link").innerHTML = 'Link: '+'<a target="_blank" href="'+curr_rep.rep_url+'">'+curr_rep.rep_url+'</a>';
+    document.querySelector(".rep_srccode").innerHTML = 'Source code: '+'<a target="_blank" href="'+curr_rep.rep_src+'">'+curr_rep.rep_src+'</a>';
+    document.querySelector(".add_notes").innerHTML = '<textarea placeholder="Additional notes for current repository(project)." oninput="add_note(\''+rep+'\')" id="add_notes'+rep+'" cols="30" rows="10"></textarea>';
+    load_note(rep);
+    document.querySelector(".main").style.display="none";
+    document.querySelector(".src_result").style.display = "none";
+    document.querySelector(".pin").style.display = "none";
+    document.querySelector(".container-for-single").style.display="block";
+}
+
+function load_note(rep){
+    let x = localStorage.getItem(rep);
+    document.querySelector("#add_notes"+rep).value = x;
+}
+
+function add_note(rep){
+    let note = document.querySelector("#add_notes"+rep).value;
+    localStorage.setItem(rep,note);
+}
+
+function single_rep_s(rep){
+    let curr_rep;
+    for(let i=0;i<stech.length;i++){
+        if(rep+".github.io"==stech[i].rep_name){
+            curr_rep = stech[i];
+        }
+    }
+    let x = '<button style="background-color:blue;color:white;border:3px solid blue;" onclick="toggle_back()"><i class="fa fa-arrow-left"></i>Back</button><button onclick="toggle_home()"><i class="fa fa-home"></i>Home</button>';
+    document.querySelector(".cfs-controls").innerHTML = x;
+    document.querySelector(".rep_name").innerHTML = 'Repository: '+curr_rep.rep_name;
+    document.querySelector(".rep_title").innerHTML = 'Title: '+curr_rep.title;
+    document.querySelector(".rep_created").innerHTML = 'Created on: '+curr_rep.rep_created;
+    document.querySelector(".rep_lstupd").innerHTML = 'Last updated: '+curr_rep.rep_lst_upd;
+    document.querySelector(".rep_link").innerHTML = 'Link: '+'<a target="_blank" href="'+curr_rep.rep_url+'">'+curr_rep.rep_url+'</a>';
+    document.querySelector(".rep_srccode").innerHTML = 'Source code: '+'<a target="_blank" href="'+curr_rep.rep_src+'">'+curr_rep.rep_src+'</a>';
+    document.querySelector(".add_notes").innerHTML = '<textarea placeholder="Additional notes for current repository(project)." oninput="add_note(\''+rep+'\')" id="add_notes'+rep+'" cols="30" rows="10"></textarea>';
+    load_note(rep);
+    document.querySelector(".main").style.display="none";
+    document.querySelector(".src_result").style.display = "none";
+    document.querySelector(".container-for-single").style.display="block";
+    document.querySelector(".main").style.display="none";
+    document.querySelector(".src_result").style.display = "none";
+    document.querySelector(".container-for-single").style.display="block";
+}
+
+function toggle_back(){
+    document.querySelector(".container-for-single").style.display = "none";
+    document.querySelector(".main").style.display = "none";
+    document.querySelector(".src_result").style.display = "block";
+}
+
+function set_mark(rep){
+    let x = document.querySelector('#'+rep+'pinbtn');
+    if(x.innerHTML == '<i class="fa fa-bookmark"></i>'){
+        x.innerHTML = '<i class="fa fa-bookmark-o"></i>';
+        localStorage.setItem(rep+'pstat',"false");
+        console.log(localStorage.getItem(rep+'pstat'));
+    }
+    else{
+        x.innerHTML = '<i class="fa fa-bookmark"></i>';
+        localStorage.setItem(rep+'pstat',"true");
+        console.log(localStorage.getItem(rep+'pstat'));
+
+    }
+}
+
+function set_mark_s(rep){
+    let x = document.querySelector('#s'+rep+'pinbtn');
+    if(x.innerHTML == '<i class="fa fa-bookmark"></i>'){
+        x.innerHTML = '<i class="fa fa-bookmark-o"></i>';
+        localStorage.setItem(rep+'pstat',"false");
+    }
+    else{
+        x.innerHTML = '<i class="fa fa-bookmark"></i>';
+        localStorage.setItem(rep+'pstat',"true");
+    }
+}
+
+function load_pinned(){
+    let thhead = '<tr><th>Name</th><th>Repository</th><th>Created</th><th>Last Updated</th><th>Action</th></tr>';
+    document.querySelector("#pin_rep").innerHTML = thhead;
+    for(let i=0;i<unsort_rep_name.length;i++){
+        if(localStorage.getItem(unsort_rep_name[i]+"pstat")=="true"){
+            let temp = '<tr><td id="'+unsort_rep_name[i]+'" style="text-align:left;" onclick="single_rep(\''+unsort_rep_name[i]+'\')">'+stech[i].title+'</td><td style="text-align:left;"><a href="'+stech[i].rep_src+'" target="_blank">'+stech[i].rep_name+'</a></td><td>'+stech[i].rep_created+'</td><td>'+stech[i].rep_lst_upd+'</td><td><a href="'+stech[i].rep_url+'" target="_blank"><button><i class="fa fa-globe"></i></button></a><button id="'+unsort_rep_name[i]+'pipinbtn" onclick="set_mark_p(\''+unsort_rep_name[i]+'\')"><i class="fa fa-bookmark"></i></button></td></tr>';
+            document.querySelector("#pin_rep").innerHTML += temp;
+        }
+    }
+}
+
+function set_mark_p(rep){
+    let x = document.querySelector('#'+rep+'pipinbtn');
+    if(x.innerHTML == '<i class="fa fa-bookmark"></i>'){
+        x.innerHTML = '<i class="fa fa-bookmark-o"></i>';
+        localStorage.setItem(rep+'pstat',"false");
+    }
+    else{
+        x.innerHTML = '<i class="fa fa-bookmark"></i>';
+        localStorage.setItem(rep+'pstat',"true");
+    }
+    load_pinned();
+}
+
+function show_pin(){
+    document.querySelector(".pinbtn").style.backgroundColor = "blue";
+    document.querySelector(".allbtn").style.backgroundColor = "black";
+    document.querySelector(".src_result").style.display = "none";
+    document.querySelector(".container-for-single").style.display = "none";
+    document.querySelector(".all").style.display = "none";
+    document.querySelector(".pin").style.display = "block";
+    load_pinned();
+}
+
+function show_all(){
+    document.querySelector(".pinbtn").style.backgroundColor = "black";
+    document.querySelector(".allbtn").style.backgroundColor = "blue";
+    document.querySelector(".src_result").style.display = "none";
+    document.querySelector(".container-for-single").style.display = "none";
+    document.querySelector(".pin").style.display = "none";
+    document.querySelector(".all").style.display = "block";
+    upd_fill();
+}
